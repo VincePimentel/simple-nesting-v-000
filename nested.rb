c463938bc58d2c2aae51c9fef6525a4fb1f6621c
Vince Andrew Pimentel <contact@vincep.me>
2018-10-17 21:22:41 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -102,11 +102,11 @@ def changing_alan
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+    }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-
-
+    alans_new_info = "GUI"
+		programmer_hash[:alan_kay][:known_for] = alans_new_info
+		programmer_hash
 end
 
 def adding_to_dennis
